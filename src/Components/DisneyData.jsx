89f4f4f7dca93e07a89b3208ba21ca17d25f55bd
reevcrona <89569507+reevcrona@users.noreply.github.com>
2024-12-02 14:30:07 +0100

    added some logic that tracks the page range so user can move pages

--- src/Components/DisneyData.jsx
+++ src/Components/DisneyData.jsx
@@ -1,6 +1,7 @@
 import {useState, useEffect } from "react";
 import axios from "axios";
 import "../styles/DisneyData.css";
+import PlaceHolderImage from "../assets/No-Image-Placeholder.svg"
 
 function DisneyData(){
     
@@ -8,52 +9,75 @@ function DisneyData(){
     const [nextPage,SetNextPage] = useState("");
     const [prevPage,setPrevPage] = useState("");
 
+    const [currentPage,setCurrentPage] = useState(1);
+    const totalPages = 372;
+
     useEffect(() => {
         axios.get(`https://api.disneyapi.dev/character?pageSize=20`).then((res) => {
             console.log(res.data)
             setDisneyData(res.data.data)
             SetNextPage(res.data.info.nextPage)
-            
+            setTotalPages(res.data.info.totalPages)
         })
     },[])
     
-    const getNextPage = () => {
-        axios.get(nextPage).then((res) => {
-            setDisneyData(res.data.data)
-            SetNextPage(res.data.info.nextPage)
-            setPrevPage(res.data.info.previousPage)
-        })
-    }
-
-    const getPrevPage = () => {
-       if(prevPage){
-            axios.get(prevPage).then((res) => {
+    
+    const getPage = (url) => {
+        if(url){
+            axios.get(url).then((res) => {
                 setDisneyData(res.data.data)
                 SetNextPage(res.data.info.nextPage)
                 setPrevPage(res.data.info.previousPage)
             })
-       }
-        
+        }
     }
     const renderData = (data) => {
         return data.map((item,index) => {
          return (
              <div key={index}>
                  <h2 className="data-name">{item.name}</h2>
-                 <img className="data-img" src={item.imageUrl}></img>
+                 <img className="data-img" src={item.imageUrl ? item.imageUrl:PlaceHolderImage}></img>
+                 <p style={{color:"white"}}>{item.films.length > 0 ? item.films:"No films"}</p>
              </div>
          )
         })
      }
 
+     const apiCallPageIndex = (index) => {
+        axios.get(`https://api.disneyapi.dev/character?page=${index}&pageSize=20`).then((res) => {
+                setDisneyData(res.data.data)
+                SetNextPage(res.data.info.nextPage)
+                setPrevPage(res.data.info.previousPage)
+                setCurrentPage(index)
+        })
+     }
+
+
+     const getPageRange = () => {
+        const start = currentPage
+        const end = start + 4 ; 
+        console.log("Start:", start, "End:", end);  
+        const numContainer = [];
+        for (let i = start; i <= end; i++) {
+            const item = <h2 className="num" onClick={() => apiCallPageIndex(i)}>{i}</h2>
+                
+            numContainer.push(item);
+        }
+        const firstPage = <h2 className="num" onClick={() => apiCallPageIndex(1)}>First page</h2>
+        const lastPage = <h2 className="num" onClick={() => apiCallPageIndex(totalPages)}>Last page</h2>
+        numContainer.unshift(firstPage)
+        numContainer.push(lastPage)
+        return numContainer;
+    };
+
     return(
         <div className="disney-data-main-container">
-           <button onClick={getPrevPage}>Prev page</button>
-           <button onClick={getNextPage} >Next page</button>
+           <button onClick={() => getPage(prevPage)}>Prev page</button>
+           <button onClick={() => getPage(nextPage)} >Next page</button>
             <div className="disney-data-container">
                 {disneyData.length > 0 ? renderData(disneyData) : <h1>No data</h1>}
             </div>
-            
+           <div className="page-range-container">{getPageRange()}</div>
         </div>
     )
 }
